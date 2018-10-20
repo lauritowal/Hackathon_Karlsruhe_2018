5d30f019459ec2aca619f5c6659fe68064ea59dd
bgokoglu <berkgokoglu@gmail.com>
2018-10-20 23:02:52 +0300

    FATURE: show map

--- Frontend/js/script.js
+++ Frontend/js/script.js
@@ -1,52 +1,60 @@
-const MULTIPLIER = 100;
+const MULTIPLIER = 9;
+const POINT_MULTIPLIER = MULTIPLIER -1;
+const POINT_X_OFFSET = 25;
+const POINT_Y_OFFSET = 30;
 
 const X_MIN = -62.5 * MULTIPLIER;
 const X_MAX = 53.6 * MULTIPLIER;
 const Y_MIN = -67.5 * MULTIPLIER;
 const Y_MAX = 14.325 * MULTIPLIER;
 
-let margin = {top: 20, right: 20, bottom: 30, left: 50},
-    width = 600 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+const VIEW_POINT_DIVIDER_X = 2;//1.5;
+const VIEW_POINT_DIVIDER_Y = 2;//1.25;
 
-let svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform",
-        "translate(" + margin.left + "," + margin.top + ")")
+var width = Math.abs(X_MIN) + Math.abs(X_MAX);
+var height = Math.abs(Y_MIN) + Math.abs(Y_MAX);
 
-let x = d3.scaleLinear().range([X_MIN, X_MAX]);
-let y = d3.scaleLinear().range([Y_MIN, Y_MAX]);
-
-x.domain([X_MIN, X_MAX]);
-y.domain([Y_MIN, Y_MAX]);
-
-var poly = [{"x":10, "y":50},
-        {"x":20,"y":20},
-        {"x":50,"y":10},
-        {"x":30,"y":30}];
+var svg = d3.select("#svgcontainer")
+   .append("svg")
+   .attr("width", width)
+   .attr("height", height)
+   .attr("viewBox", `${-width/VIEW_POINT_DIVIDER_X} ${-height/VIEW_POINT_DIVIDER_Y} ${width} ${height}`)
+   .style("border", "1px solid black")
+   .style("stroke-width", 2);
 
 
-  svg.selectAll("polygon")
-    .data([poly])
-  .enter().append("polygon")
-    .attr("points",function(d) {
-        return d.map(function(d) {
-            return [x(d.x),y(d.y)].join(",");
-        }).join(" ");
+AREAS.forEach(area => {
+  try {
+      let shape = JSON.parse(area.shape);
+      let points = "";
+      shape.coordinates.forEach(points => {
+
+      points = points.map(point => [(point[0]+(POINT_X_OFFSET))*POINT_MULTIPLIER , (point[1]+(POINT_Y_OFFSET))*POINT_MULTIPLIER]);
+
+      svg.append("polygon")
+        .attr("points", points)
+        .style("fill", "none")
+        .style("stroke", "black")
+        .style("strokeWidth", "10px");
+      
+
     });
+  } catch(e) {
+    console.warn("JSON is not valid:", e)
+  }
+});
 
-  svg.append("circle")
-    .attr("r", 4)
-    .attr("cx", x(point.x))
-    .attr("cy", y(point.y))
+/*svg.append("line")
+   .attr("x1", -width/VIEW_POINT_DIVIDER_X)
+   .attr("y1", 0)
+   .attr("x2", width/VIEW_POINT_DIVIDER_X)
+   .attr("y2", 0)  
+   .style("stroke", "rgb(255,0,0)");
 
-    // add the X Axis
-  svg.append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x));
 
-  // add the Y Axis
-  svg.append("g")
-    .call(d3.axisLeft(y));
+svg.append("line")
+.attr("x1", 0)
+.attr("y1", -height/VIEW_POINT_DIVIDER_Y)
+.attr("x2", 0)
+.attr("y2", height/VIEW_POINT_DIVIDER_Y)  
+.style("stroke", "rgb(255,0,0)");*/
